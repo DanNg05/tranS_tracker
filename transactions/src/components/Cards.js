a0554fba1cef993edc7029b944a47567926cfc76
Dan Nguyen <sydan05071997@gmail.com>
2024-06-19 22:49:08 +0930

    stuck at axios with link

--- transactions/src/components/Cards.js
+++ transactions/src/components/Cards.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+const API_URL = "http://localhost:3000/api/v1/cards";
 
+function getAPIData () {
+  return axios.get(API_URL).then((response) => response.data)
+}
 const Cards = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/cards')
+      .then(response => {
+        setCards(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error("There was an error fetching the posts!", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Cards Page</h1>
--- transactions/src/index.js
+++ transactions/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
